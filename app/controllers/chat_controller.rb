0a7b9f1327165a50365339ba3704e9be730fdf7a
Yongchol Choi <charleschoi87@gmail.com>
2015-08-25 07:40:43 +0000

    websocket setting

---
+++ app/controllers/chat_controller.rb
@@ -0,0 +1,11 @@
+class ChatController < WebsocketRails::BaseController
+  def initialize_session
+    # perform application setup here
+    controller_store[:message_count] = 0
+  end
+  
+  def echo
+    receive_message = message()
+    broadcast_message(:echo, receive_message)
+  end
+end
